2443d4f8c9d3e2642b2b7659f9331de67f6a74d6
Josue Perez <a6523150022@utch.edu.mx>
2025-03-06 20:55:40 -0600

    Add the basic structure of index. Add the instances of the form's components.

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,45 +1,47 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // data of origin
     var optionsOrigin = {
-        data:{
+        data: {
             "Chihuahua": '/static/img/low_quality/low_chihuahua.jpg',
         }
     }
     // data of places to go
     var optionsDestiny = {
-        data:{
-            "CDMX": null,
-            "Monterrey": null,
-            "Puebla": null,
-            "Cancun": '/static/img/low_quality/low_cancun.jpg',
+        data: {
+            "Playa": null,
+            "Bosque": null,
+            "Monumento": null,
+            "Pueblo": null,
         }
     }
     var origin_autocomplete = document.querySelectorAll('.origin_autocomplete');
-    var destiny_autocomplete = document.querySelectorAll('.destiny_autocomplete')
+    var place_autocomplete = document.querySelectorAll('.place_autocomplete')
 
     var instances = M.Autocomplete.init(origin_autocomplete, optionsOrigin);
-    var instances = M.Autocomplete.init(destiny_autocomplete, optionsDestiny);
+    var instances = M.Autocomplete.init(place_autocomplete, optionsDestiny);
 
-    // --------------------------------------------------//
-    var style = {
-        format: 'yyyy-mm-dd',
-        i18n: {
-            cancel: 'Cancel',
-            clear: 'Clear',
-            done: 'Done',
-            months: ['January', 'February', 'March', 'April', 'May', 'Jun', 'July',
-                'August', 'September', 'October', 'November', 'Dicember'
-            ],
-            monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Agu',
-                'Sep', 'Oct', 'Nov', 'Dec'
-            ],
-            weekdays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
-                'Saturday'
-            ],
-            weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
-            weekdaysAbbrev: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+    // -------------------SELECTS---------------------------//
+    var weather_select = document.querySelectorAll('.weather_select');
+    var season_select = document.querySelectorAll('.season_select');
+
+    var optionsWeather = {
+        data: {
+            "Caluroso": null,
+            "Frio": null,
+            "Templado": null,
+            "Nieve": null,
         }
     }
-    var datePicker = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(datePicker, style);
+
+    var optionsSeason = {
+        classes: 'custom-select',  // Clase CSS personalizada
+        dropdownOptions: {
+            alignment: 'right',  // Alinea el desplegable a la derecha
+            autoTrigger: false,  // Evita que se abra automáticamente
+            coverTrigger: false, // Permite que el desplegable sobresalga del botón
+            closeOnClick: false  // No se cierra al hacer clic en una opción
+        }
+    }
+    var instances = M.FormSelect.init(weather_select, optionsWeather);
+    var instances = M.FormSelect.init(season_select, optionsSeason);
 });
