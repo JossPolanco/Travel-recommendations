c68cce86714d48c415d91a65647834d5eb6333c6
Josue Perez <a6523150022@utch.edu.mx>
2025-03-09 19:18:45 -0600

    Add the basic structure of the map. Add basic styles.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config.db import init_db
 from routes import index, search, place
+from mapkick.flask import mapkick_blueprint
 
 app = Flask(__name__)
 mysql = init_db(app)
@@ -8,6 +9,8 @@ mysql = init_db(app)
 app.register_blueprint(index.bp)
 app.register_blueprint(search.bp)
 app.register_blueprint(place.bp)
+app.register_blueprint(mapkick_blueprint)
+
 
 if __name__ == "__main__":
     app.run(port=9000, debug=True)
