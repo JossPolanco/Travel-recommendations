8561d475267ec3aba8d48fa5bd7331803180da03
Josue Perez <a6523150022@utch.edu.mx>
2025-03-11 19:12:44 -0600

    Add more options.

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -2,7 +2,38 @@ document.addEventListener('DOMContentLoaded', function () {
     // data of origin
     var optionsOrigin = {
         data: {
+            "Aguascalientes": null,
+            "Baja California": null,
+            "Baja California Sur": null,
+            "Campeche": null,
+            "Chiapas": null,
             "Chihuahua": null,
+            "Ciudad de México": null,
+            "Coahuila": null,
+            "Colima": null,
+            "Durango": null,
+            "Estado de México": null,
+            "Guanajuato": null,
+            "Guerrero": null,
+            "Hidalgo": null,
+            "Jalisco": null,
+            "Michoacán": null,
+            "Morelos": null,
+            "Nayarit": null,
+            "Nuevo León": null,
+            "Oaxaca": null,
+            "Puebla": null,
+            "Querétaro": null,
+            "Quintana Roo": null,
+            "San Luis Potosí": null,
+            "Sinaloa": null,
+            "Sonora": null,
+            "Tabasco": null,
+            "Tamaulipas": null,
+            "Tlaxcala": null,
+            "Veracruz": null,
+            "Yucatán": null,
+            "Zacatecas": null
         }
     }
     // data of places to go
